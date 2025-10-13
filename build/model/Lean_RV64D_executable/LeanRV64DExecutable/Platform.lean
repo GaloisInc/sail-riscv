cf941c236a60f366e2aa01f1165981eef32e0472
jn80842 <newcomb@galois.com>
2025-10-12 20:36:11 -0700

    more plat_write_term debugging

--- build/model/Lean_RV64D_executable/LeanRV64DExecutable/Platform.lean
+++ build/model/Lean_RV64D_executable/LeanRV64DExecutable/Platform.lean
@@ -648,6 +648,7 @@ def htif_store (app_0 : physaddr) (width : Nat) (data : (BitVec (8 * width))) : 
               then (pure ())
               else
                 (do
+                  dbg_trace "probably about to call plat_term_write"
                   if ((b__2 == (0x01 : (BitVec 8))) : Bool)
                   then (plat_term_write (Sail.BitVec.extractLsb (_get_htif_cmd_payload cmd) 7 0))
                   else
@@ -704,4 +705,3 @@ def init_platform (_ : Unit) : SailM Unit := do
 
 def platform_wfi (_ : Unit) : Unit :=
   ()
-
